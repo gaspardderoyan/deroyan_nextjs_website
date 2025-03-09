c85f082e60e47e3a9f077b0fca843bd08338dc79
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2025-03-09 18:27:04 +0000

    about to re-do the api files for items

--- app/lib/index.ts
+++ app/lib/index.ts
@@ -18,6 +18,7 @@ export interface ImageData {
     thumbnail?: { url: string; width: number; height: number };
     small?: { url: string; width: number; height: number };
     medium?: { url: string; width: number; height: number };
+    large?: {url: string; width: number; height: number};
   };
 }
 
